717c1f6cb6cca873c2096d324f5779f899c9ce20
hachmimohamed <31771826+hachmimohamed@users.noreply.github.com>
2026-06-27 13:33:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# Assurez-vous que DATABASE_URL est bien dans l'onglet Environment de Render
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db_connection():
@@ -51,16 +52,19 @@ def get_score():
 def tap():
     data = request.json
     uid = str(data.get('telegram_id'))
-    bonus = int(data.get('bonus', 1)) # Prend 1 par défaut, ou 600 si envoyé
+    # Récupération du bonus envoyé (600 pour la mission, sinon 1 par défaut)
+    bonus = int(data.get('bonus', 1)) 
     try:
         conn = get_db_connection()
         cur = conn.cursor()
         
+        # Mise à jour avec le bonus dynamique
         cur.execute("UPDATE users SET points = points + %s WHERE id = %s", (bonus, uid))
         if cur.rowcount == 0:
             cur.execute("INSERT INTO users (id, points) VALUES (%s, %s)", (uid, bonus))
         
         conn.commit()
+        
         cur.execute("SELECT points FROM users WHERE id = %s", (uid,))
         new_score = cur.fetchone()[0]
         
