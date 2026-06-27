1f066711867f1372c6b9edf7c7fd1026a493d1bd
hachmimohamed <31771826+hachmimohamed@users.noreply.github.com>
2026-06-27 13:27:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Assurez-vous que DATABASE_URL est bien dans l'onglet Environment de Render
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db_connection():
-    # Connexion à la base de données
     return psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# Initialisation automatique de la table au démarrage
 def init_db():
     try:
         conn = get_db_connection()
@@ -54,18 +51,16 @@ def get_score():
 def tap():
     data = request.json
     uid = str(data.get('telegram_id'))
+    bonus = int(data.get('bonus', 1)) # Prend 1 par défaut, ou 600 si envoyé
     try:
         conn = get_db_connection()
         cur = conn.cursor()
         
-        # Mise à jour ou insertion de l'utilisateur avec tuple correct (uid,)
-        cur.execute("UPDATE users SET points = points + 1 WHERE id = %s", (uid,))
+        cur.execute("UPDATE users SET points = points + %s WHERE id = %s", (bonus, uid))
         if cur.rowcount == 0:
-            cur.execute("INSERT INTO users (id, points) VALUES (%s, 1)", (uid,))
+            cur.execute("INSERT INTO users (id, points) VALUES (%s, %s)", (uid, bonus))
         
         conn.commit()
-        
-        # Récupération du nouveau score
         cur.execute("SELECT points FROM users WHERE id = %s", (uid,))
         new_score = cur.fetchone()[0]
         
@@ -73,7 +68,6 @@ def tap():
         conn.close()
         return jsonify({"new_score": new_score})
     except Exception as e:
-        # Affichage de l'erreur dans les logs de Render pour débogage
         print(f"Erreur API TAP : {str(e)}")
         return jsonify({"error": str(e)}), 500
 
