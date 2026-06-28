a7168ebdcba29107bd7aa7dc6227b8369f7e2518
hachmimohamed <31771826+hachmimohamed@users.noreply.github.com>
2026-06-28 11:42:15 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -93,5 +93,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    # Utilisation du port 10000 par défaut pour Render
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
